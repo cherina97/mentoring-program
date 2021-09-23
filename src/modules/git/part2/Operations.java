5fba541f27da2fb5c3670661aa63c5e7146c3311
cherina97 <cherina97@gmail.com>
2021-09-23 10:31:42 +0300

    implemented subtraction operation

--- src/modules/git/part2/Operations.java
+++ src/modules/git/part2/Operations.java
@@ -3,9 +3,14 @@ package modules.git.part2;
 public class Operations {
     public static void main(String[] args) {
         System.out.println(add(1, 2));
+        System.out.println(subtraction(1, 2));
     }
 
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static int subtraction(int a, int b) {
+        return a - b;
     }
 }
