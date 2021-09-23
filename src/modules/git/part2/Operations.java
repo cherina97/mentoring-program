dcf9d4e001760ee778b958267c8b7e6968caba98
cherina97 <cherina97@gmail.com>
2021-09-23 10:27:18 +0300

    implemented add operation

---
+++ src/modules/git/part2/Operations.java
@@ -0,0 +1,11 @@
+package modules.git.part2;
+
+public class Operations {
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
