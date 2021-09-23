b59b8a0c8a2c81141dd2f00684f6fa3fb9e85664
cherina97 <cherina97@gmail.com>
2021-09-23 10:32:21 +0300

    implemented division operation

--- src/modules/git/part2/Operations.java
+++ src/modules/git/part2/Operations.java
@@ -4,6 +4,7 @@ public class Operations {
     public static void main(String[] args) {
         System.out.println(add(1, 2));
         System.out.println(subtraction(1, 2));
+        System.out.println(division(1, 2));
     }
 
     public static int add(int a, int b) {
@@ -12,5 +13,9 @@ public class Operations {
 
     public static int subtraction(int a, int b) {
         return a - b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
     }
 }
